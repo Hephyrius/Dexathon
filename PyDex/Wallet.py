09f87f2407d48ffd9821ff081042cf1d070701b1
Harnick Khera <Hephyrius@gmail.com>
2018-06-23 04:54:16 +0100

    Added Basic Key-Pair Creation

--- PyDex/Wallet.py
+++ PyDex/Wallet.py
@@ -5,17 +5,22 @@ Created on Sat Jun 23 04:00:04 2018
 @author: Khera
 """
 
-#requries pip install crypto
-from crypto.PublicKey import RSA
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
 
 class Wallet():
      
      PrivateKey = ""
+     PEMKey
      PublicKey = ""
      
      def __init__(self):
           
-          self.PrivateKey = RSA.generate(2048)
-          self.PublicKey = self.PrivateKey.publickey()
+          self.PrivateKey = rsa.generate_private_key(backend=default_backend(), public_exponent=65537, key_size=2048)
+          self.PublicKey = self.PrivateKey.public_key().public_bytes(serialization.Encoding.OpenSSH, serialization.PublicFormat.OpenSSH)
+          self.PEMKey  = self.PrivateKey.private_bytes(encoding=serialization.Encoding.PEM,format=serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm=serialization.NoEncryption())
           
           
+wallet = Wallet()
+print(wallet.PEMKey.decode('utf-8'))
